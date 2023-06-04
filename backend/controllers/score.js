3a5deb7748c9a12e9a4f3335c114d79dfdb52129
harshalvaidya18@gmail.com <harshalvaidya@gmail.com>
2023-06-04 22:09:25 +0530

    added donut charts, there is time lag while loading data, new company data is loading on top of old company data

--- backend/controllers/score.js
+++ backend/controllers/score.js
@@ -1,30 +1,83 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: 'harshal',
-    host: 'localhost',
-    database: 'eqax_db',
-    password: 'test123',
-    port: 5432,
+const mysql = require('mysql')
+const pool  = mysql.createPool({
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'stockscore',
+    port            :3306
 })
 
 exports.getData = (req,res)=>{
-    pool.query('SELECT * FROM SCORES', (error, results) => {
+    pool.query('SELECT name,fcf,cfo,profit_ttm,profit,sales FROM stocks', (error, results) => {
         if (error) {
             throw error
         }
-        res.status(200).json(results.rows)
+        console.log(JSON.stringify(results))
+        res.status(200).json(results)
     })
 }
 
 exports.getCompanyByCode = (req, res) => {
     const code = req.params.companyCode
-    pool.query('SELECT * FROM SCORES WHERE company_code = $1', [code], (error, results) => {
+    pool.query('SELECT name,stockscrore FROM stocks WHERE name = ?', [code], (error, results) => {
         if (error) {
-            throw error
+            return res.status(400).json({
+                error: "No Company exists by company code"
+              });
         }
-        
-        res.status(200).json(results.rows)
+       
+        res.status(200).json(results)
 })
 
+}
+
+
+exports.getStockByName = (req,res)=>{
+    const companyName = req.params.companyName
+  
+    pool.query('SELECT name FROM stocks WHERE name LIKE ? LIMIT 5 ', [companyName.concat('%')],(error, results) => {
+        if (error) {
+            return res.status(400).json({
+                error: "No Company exists by Stock by name"
+              });
+        }
+       
+       
+        res.status(200).json(results)
+})
+
+}
+
+exports.getSevenScores = (req, res)=>{
+
+    const companyName = req.params.companyName
+  
+    pool.query('SELECT price,profitibility, scale,debt,confidence,cashflow,growth FROM `stocks` WHERE name =? ', [companyName],(error, results) => {
+        if (error) {
+            return res.status(400).json({
+                error: "No 7 score exist exists by Stock by name"
+              });
+        }
+       
+       
+        res.status(200).json(results)
+
+})
+}
+
+exports.getColor=(req,res)=>{
+    const score = req.params.score
+
+    pool.query('SELECT code from colors where number = ? ', [score],(error, result) => {
+        if (error) {
+            return res.status(400).json({
+                error: "no color found"
+              });
+        }
+       
+       
+        res.status(200).json(result)
+
+})
 }
 
--- backend/routes/score.js
+++ backend/routes/score.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var router = express.Router();
-const {getData,getCompanyByCode} = require("../controllers/score")
+const {getData,getCompanyByCode,getStockByName,getSevenScores,getColor} = require("../controllers/score")
 
 // router.param("companyCode",getCompanyCode)
 
 router.get("/getData", getData);
 router.get("/getCompany/:companyCode",getCompanyByCode);
+router.get("/findStock/:companyName",getStockByName);
+router.get("/getSevenScores/:companyName",getSevenScores);
+router.get("/getColor/:score",getColor);
 
 
 
